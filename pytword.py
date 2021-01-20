48d6b7e5e48db5a78a9af3f2dbcf5bcd2d8bbcac
SteveontheMoon <73540419+SteveontheMoon@users.noreply.github.com>
2021-01-20 16:20:31 +0100

    init

---
+++ pytword.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import random
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+
+print('\tPYTWORD\n')
+numbers = input('Choose the numbers of password to print: (up to 4)')
+numbers = int(numbers)
+length = input('Choose the password lenght: (1 to 32) ')
+length = int(length)
+
+for p in range(numbers):
+  password = ''
+  for c in range(length):
+    password += random.choice(chars)
+  print( password)
+
+
+prova
